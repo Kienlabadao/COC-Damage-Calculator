e175fd4bd355fbeb4e79d861b725b571bf8a466a
Kienlabadao <87053094+Kienlabadao@users.noreply.github.com>
2025-01-22 22:11:45 +0900

    🔥Remove duplicate & unused files

--- src/features/setting/actions/clearStorage/clearStorage.tsx
+++
@@ -1,7 +0,0 @@
-import { toast } from "react-toastify";
-import { clearAll } from "utils/localStorage";
-
-export function clearStorage() {
-  clearAll();
-  toast("Hello! This is a default notification.");
-}
